63f82c2bea551a7685c625f42ca916ada39193ee
Mohamed yahya <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2022-10-12 10:03:48 +0100

    Prototype Standard Start

---
+++ Prototype Standard/Data-Access/promotion.php
@@ -0,0 +1,27 @@
+<?php
+class promo {
+
+    private $id;
+    private $Name;
+
+    public function getId (){
+        return $this->Id;
+    }
+
+    public function setId($id){
+        $this->Id = $id;
+    }
+
+    public function getName (){
+        return $this->Name;
+    }
+
+    public function setName($Name){
+        $this->Name = $Name;
+    }
+
+
+}
+
+
+?>
